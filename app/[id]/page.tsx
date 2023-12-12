6774e5ee4d1289495b9831150643497014950a1e
Svitlana <rsv0011@gmail.com>
2023-12-12 15:28:37 +0100

    feat: add the product page

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -4,6 +4,7 @@ import Card from "../components/Card";
 const page = async ({ params }: { params: { id: string } }) => {
   const data = (await client.getEntry(params.id)).fields;
 
+
   return (
     <div className="flex items-center flex-col">
       <h1 className="p-20">Your Item</h1>
