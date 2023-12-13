c3ef9f321bf67b112742aefafbdb5b85f84a3b47
Antti Jankeri <antti.jankeri@gmail.com>
2023-12-13 15:03:30 +0100

    feat: working features just need styling

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -22,8 +22,11 @@ const Card: React.FC<CardProps> = ({
     <>
       <Link
         href={"/" + id}
-        className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <Image src={url} alt={description} width={500} height={500} />
+        className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      >
+        <div className="w-50 h-40 relative">
+          <Image src={url} alt={description} width={100} height={100} />
+        </div>
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {name}
         </h5>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 import Card from "./components/Card";
 import axios from "axios";
 
@@ -10,23 +9,33 @@ const fetchProducts = async () => {
 
 export default async function Home() {
   const products = await fetchProducts();
+
   return (
     <ul className="flex flex-wrap items-center justify-center">
-      {products.map((product) => {
-        return (
-          <li key={(product.id + "li") as string}>
-            <Card
-              key={product.id as string}
-              name={product.name as string}
-              description={product.description as string}
-              price={product.price as number}
-              url={product.image}
-              quantity={product.quantity}
-              id={product.id}
-            />
-          </li>
-        );
-      })}
+      {products.map(
+        (product: {
+          id: string;
+          name: string;
+          description: string;
+          price: number;
+          image: string;
+          quantity: number;
+        }) => {
+          return (
+            <li key={(product.id + "li") as string}>
+              <Card
+                key={product.id as string}
+                name={product.name as string}
+                description={product.description as string}
+                price={product.price as number}
+                url={product.image}
+                quantity={product.quantity}
+                id={product.id}
+              />
+            </li>
+          );
+        }
+      )}
     </ul>
   );
 }
